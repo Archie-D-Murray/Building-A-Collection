47fc709f6ea4ad550347d40dfb6b71811725d25f
Archie Murray <archie.murray@outlook.com>
2025-02-25 10:38:12 +0000

    fix: Updated build scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-clang++ -o Building-A-Collection.exe .\main.cpp .\src\*.cpp -I .\raylib-win\include\ .\raylib-win\lib\raylib.lib -lmsvcrt -lshell32 -luser32 -lopengl32 -lwinmm -lgdi32 -nostdlib -std=c++20 -g -O0
+clang++ -o Building-A-Collection.exe .\main.cpp .\src\*.cpp -I .\raylib-5.5\include\ .\raylib-5.5\lib\raylib.lib -lmsvcrt -lshell32 -luser32 -lopengl32 -lwinmm -lgdi32 -nostdlib -std=c++20 -g -O0
 if %ERRORLEVEL% == 0 Building-A-Collection.exe
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-clang++ ./main.cpp ./src/*.cpp -I ./raylib-linux/include/ ./raylib-linux/lib/libraylib.a -g -O0
+clang++ ./main.cpp ./src/*.cpp -I ./raylib-5.5/include/ ./raylib-5.5/lib/libraylib.a -g -O0
 if [ $? -eq 0 ]; then
     echo "Compile Succeeded at $(date)"
     mv ./a.out ./Making-A-Collection
